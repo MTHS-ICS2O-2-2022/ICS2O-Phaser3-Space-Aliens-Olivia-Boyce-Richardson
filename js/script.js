6d99c0367e893d6428ca298587536f95c52cba17
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-18 16:56:35 +0000

    html and js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+/* global Phaser */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+const game = new Phaser.Game(config)
+console.log(game)
