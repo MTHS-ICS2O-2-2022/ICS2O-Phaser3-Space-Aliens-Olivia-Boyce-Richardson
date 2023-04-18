325c60626cb041cf21a178de67598922eeb4ae6b
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-18 17:14:02 +0000

    html and js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
+/* global Phaser
+// Copyright (c) 2020 Mr. Coxall All rights reserved //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
-/* global Phaser */
+// This is the Phaser 3 game configuration file
+/**
+* Start Phaser Game.
+*/
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
+
 const game = new Phaser.Game(config)
 console.log(game)
