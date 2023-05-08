554c9817ad93bb0dbe22e34698c64345b9194781
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-08 17:15:46 +0000

    gance scene changes

--- js/gameScene.js
+++ js/gameScene.js
@@ -18,14 +18,9 @@ class GameScene extends Phaser.Scene {
 
     this.background = null 
     this.ship = null
+    this.fireMissile = false
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * This method is called by the Scene Manager when the scene starts,
-   *before preload() and create().
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
@@ -39,30 +34,24 @@ class GameScene extends Phaser.Scene {
 
     this.load.image("starBackground", "./assets/starBackground.png") 
     this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "./assets/missile.png")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
-    this.background = 
-    this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    this.missileGroup = this.physics.add.group()
   }
 
-  /**
-   * Should be overridden by your own Scenes.
-   * This method is called once per game step while the scene is running.
-   * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms since the last frame.
-   */
   update(time, delta) {
+    // Called 60 times a second, hopefully!
+
     const keyLeftObj = this.input.keyboard.addKey("LEFT") 
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -75,6 +64,14 @@ class GameScene extends Phaser.Scene {
       this.ship.x += 15
       if (this.ship.x > 1920) { 
       this.ship.x = 1920
+      }
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        this.fireMissile = true
+        const aNewMissile = this. physics.add.sprite(this.ship.x, this.ship.y, "missile")
+        this.missileGroup.add(aNewMissile)
       }
     }
   }
