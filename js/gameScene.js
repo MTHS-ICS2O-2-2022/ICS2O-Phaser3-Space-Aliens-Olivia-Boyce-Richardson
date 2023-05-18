26460b366b567bf7b53b388149ef220fb3f1c158
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-18 17:22:44 +0000

    chnages

--- js/gameScene.js
+++ js/gameScene.js
@@ -48,6 +48,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("alien", "./assets/alien.png")
     // sound 
     this.load.audio("laser", "./assets/laser1.wav")
+    this.load.audio("explosion", "./assets/barrelExploding.wav")
   }
 
   create(data) {
@@ -60,6 +61,14 @@ class GameScene extends Phaser.Scene {
 
     this.alienGroup = this.physics.add.group()
     this.createAlien()
+
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missile, alien) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play("explosion")
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
 
   update(time, delta) {
